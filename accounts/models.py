23d01469587c05dc63611083af3ea67fefc091d6
bravin99 <bravinshumwe.coding@gmail.com>
2022-10-26 20:11:48 +0300

    authentication and user models

---
+++ accounts/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class AccountBaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+EDU_CHOICES = (
+    (1, "Certificate"),
+    (2, "Diploma"),
+    (3, "Degree"),
+    (4, "Masters"),
+    (5, "Masters & MBA")
+)
+
+class Profile(AccountBaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    confirmed_email = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=100)
+    alternative_email = models.EmailField()
+    registered_id = models.CharField(max_length=15, verbose_name="ID/Alien Number")
+    education = models.TextField(max_length=20, choices=EDU_CHOICES, default=1)
+    experience = models.CharField(max_length=10, help_text="put an acurate represantation of your experience")
+    date_of_birth = models.DateTimeField(null=True, blank=True)
+    town = models.CharField(max_length=65, null=True, blank=True)
+    nationality = models.CharField(max_length=35, null=True, blank=True)
+    current_location = models.CharField(max_length=155, null=True, blank=True)
+    prefered_work_location = models.CharField(max_length=155, null=True, blank=True)
+    languages = models.CharField(max_length=100, help_text="comma separated", null=True, blank=True)    
+    
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+    
+@receiver(post_save, sender=User)
+def update_profile_user(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
---
+++ accounts/views.py
@@ -0,0 +1,51 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from accounts.tokens import account_activation_token
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from accounts.forms import SignUpForm
+import six
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate Your Talent Portal Account'
+            message = render_to_string('registration/emails/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+            return redirect("account_activation_sent")
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def account_activation_sent(request):
+    return render(request, 'registration/account_activation_sent.html')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_encode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.confirmed_email = True
+        user.save()
+        login(request, user)
+        return redirect("/")
+    else:
+        return render(request, 'registration/account_activation_invalid.html')
