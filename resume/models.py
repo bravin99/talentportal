6c3dd9628d9af57cc7d23c4e03a0565c2368c1e5
bravin99 <bravinshumwe.coding@gmail.com>
2022-10-28 21:04:31 +0300

    Resume Document model

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,4 @@
+from random import choices
 from tabnanny import verbose
 from django.db import models
 from django.contrib.auth import get_user_model
@@ -27,3 +28,23 @@ class Experience(ResumeBaseModel):
     salary = models.DecimalField(decimal_places=2, max_digits=9, verbose_name="Salary (Ksh)")
     description = models.TextField(verbose_name="Brief description of your roles")
     why_left = models.TextField(verbose_name="Reasons for leaving")
+
+class Referee(ResumeBaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="referees")
+    referee_name = models.CharField(max_length=155)
+    organization_name = models.CharField(max_length=155)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(null=True, blank=True)
+
+def document_upload_path(instance, filename):
+    return f'application-docs/{instance.user.username}/{filename}'
+class Document(ResumeBaseModel):
+    DOC_TYPES = (
+        ('resume', 'Resuee'),
+        ('cv', 'Curriculum Vitae'),
+        ('Academic Certificates')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="documents")
+    doc_type = models.CharField(choices=DOC_TYPES, default='resume', verbose_name="Document Type")
+    doc_name = models.CharField(max_length=40, verbose_name="Document Name")
+    file = models.FileField(upload_to=document_upload_path, versose_name="Document(PDF)")
