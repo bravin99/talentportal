fcc834f37560da2eb5f2c4a0d97b4d55aa7467b3
bravin99 <bravinshumwe.coding@gmail.com>
2022-10-28 22:28:41 +0300

    resume forms

---
+++ resume/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from resume.models import Document, Education, Experience, Referee
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class EducationForm(forms.ModelForm):
+    class Meta:
+        model = Education
+        exclude = ['user', 'created', 'updated']
+
+class ExperienceForm(forms.ModelForm):
+    class Meta:
+        model = Experience
+        exclude = ['user', 'created', 'updated']
+
+class RefereeForm(forms.ModelForm):
+    class Meta:
+        model = Referee
+        exclude = ['user', 'created', 'updated']
+
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        exclude = ['user', 'created', 'updated']
